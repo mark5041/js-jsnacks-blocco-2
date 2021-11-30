2d5d3ce023ca8c40a1e0149078d04c9fb3fa6529
mark5041 <mark.beloso5041@gmail.com>
2021-11-30 08:25:32 +0100

    jsnack 1 completo

--- jsnack 1/js/script.js
+++ jsnack 1/js/script.js
@@ -1,6 +1,16 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista
 // di cognomi, Gatsby vuole generare una falsa lista di invitati.
 
+let nome = ['luca', 'giovanni', 'enrico', 'vittorio', 'umberto', 'matteo'];
+let cognome = ['ferrari', 'armani', 'emanuele', 'benedetto', 'leopardi', 'mattei'];
 
+let genera = document.querySelector('.btn');
+
+genera.addEventListener('click', 
+    function()
+    {
+        alert(nome[Math.floor(Math.random() * nome.length)] + ' ' + cognome[Math.floor(Math.random() * cognome.length)])
+    }
+);
 
 
