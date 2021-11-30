25afcb39733f89c68a70b20d8a7d0e695afdfb78
mark5041 <mark.beloso5041@gmail.com>
2021-11-30 18:04:01 +0100

    add function

--- jsnack 1/js/script.js
+++ jsnack 1/js/script.js
@@ -8,23 +8,31 @@ let max = parseInt(prompt("inserisci il numero di invitati"));
 
 let genera = document.querySelector('.btn');
 
+function ListaInvitati(number, array1, array2)
+{
+    let i = 0
+    let invitati = [];
+    while(i < number)
+    {
+        let names = array1[Math.floor(Math.random() * array1.length)] + ' ' + array2[Math.floor(Math.random() * array2.length)];
+        if(!invitati.includes(names))
+        {
+            invitati[i] = names;
+            i++;
+        }
+        
+    }
+    return invitati;
+}
+
 genera.addEventListener('click', 
     function()
     {
         let lista = document.querySelector(".lista");
-        let generate_name = [];
-        for(let i = 0; i < max; i++)
+        let ArrayInvitati = ListaInvitati(max, nome, cognome);
+        for(let i = 0; i < ArrayInvitati.length; i++)
         {
-            let names = nome[Math.floor(Math.random() * nome.length)] + ' ' + cognome[Math.floor(Math.random() * cognome.length)];
-            if(!generate_name.includes(names))
-            {
-                generate_name[i] = names;
-                lista.innerHTML += `<li> ${generate_name[i]} </li>`;
-            }
-            else
-            {
-                i--;
-            }
+                lista.innerHTML += `<li> ${ArrayInvitati[i]} </li>`;
         }
     }
 );
